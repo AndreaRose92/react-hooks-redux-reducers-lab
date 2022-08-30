7d5f154c6d79837e36fbff5c95a689b8616b5eaa
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-30 14:35:02 -0700

    commit tree dbdda8c99b836e5c2d894116393c5f2a28271040

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: []}
+let action = {
+  type: "friends/add",
+  payload: {
+    name: "",
+    hometown: '',
+    id: 0
+  }
+}
+
+export function manageFriends(state = initialState, action) {
+  if (action.type === "friends/add") {
+    return { friends: [...state.friends, action.payload] }
+  } else if (action.type === "friends/remove") {
+    return { friends: state.friends.filter( f => f.id !== action.payload ) }
+  }
+  return state
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,9 @@
 const initialState = { numberOfPresents: 0 };
+let action = { type: "presents/increase"}
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  if (action.type === "presents/increase") {
+    return { numberOfPresents: state.numberOfPresents + 1 }
+  }
+  return state
 }
